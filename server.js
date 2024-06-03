76d631de4bfe0e4696e9ed50edbcc8e74b83f18c
ManoharPulluru1 <manu.manohar@gaiansolutions.com>
2024-06-03 11:51:51 +0530

    modifed to mapbox

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: "*" } });
 
 const PORT = process.env.PORT || 4000;
 
-const formatDateTime = (date) => {
-  return date.toLocaleString("en-GB", {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  });
-};
+let users = [];
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
-  
-  socket.on('newMessage', (data) => {
-    const currentDate = new Date();
-    const formattedDateTime = formatDateTime(currentDate);
-    const messageWithDateTime = {
-      message: data,
-      dateTime: formattedDateTime,
-    };
-    io.emit('notification', messageWithDateTime);
-    console.log(`Received and emitted new message: ${data} at ${formattedDateTime}`);
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.emit("users", users);
+
+  socket.on("addUser", (data) => {
+    users.push({
+      name: data.username,
+      lat: data.lat,
+      lng: data.lng,
+      index: users.length,
+    });
+    console.log(users, "addUser");
+    io.emit("users", users);
   });
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+  socket.on("updateUserMessage", (index, message) => {
+    console.log(index, message, "updateUserMessage")
+    users[index].message = message;
+    io.emit("users", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
 
-app.use(cors()); // Enable CORS for all routes
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
