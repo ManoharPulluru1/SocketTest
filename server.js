8621c0d58deb08a1eee193c8c3d76c752a216237
ManoharPulluru1 <manu.manohar@gaiansolutions.com>
2024-06-05 10:12:05 +0530

    removed all users

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ const PORT = process.env.PORT || 4000;
 io.on("connection", (socket) => {
   console.log("New client connected");
 
+  socket.emit("users", users);
+
   socket.on("addAnUser", (user) => {
     if (!users.find(u => u.mobile === user.mobile)) {
       users.push(user);
       socket.emit("users", users);
     }
   });
-  
 
-  socket.emit("users", users);
 
   socket.on("getUserName", (mobile) => {
     const user = users.find((user) => user.mobile === mobile);
@@ -32,7 +32,7 @@ io.on("connection", (socket) => {
       socket.emit("userName", user.userName);
       console.log(user.userName, "-------------user");
     } else {
-      socket.emit("userName", null); // Emit null or an appropriate message if the user is not found
+      socket.emit("userName", null);
     }
   });
 
@@ -41,20 +41,6 @@ io.on("connection", (socket) => {
     socket.emit("existingUser", !!existingUser);
   });
 
-  socket.on("updateUserLocation", ({ userLocation, mobile }) => {
-    console.log(userLocation, mobile);
-    const user = users.find((user) => user.mobile === mobile);
-    console.log(user, "=====> user before update")
-    console.log(users, "=====> users before update");
-    if (user) {
-      user.lat = userLocation.lat;
-      user.lng = userLocation.lng;
-      console.log(user, "=====> user after update")
-      console.log(users, "=====> users after update");
-
-      socket.emit("userLocation", { mobile, userLocation });
-    }
-  });
 
   socket.on("disconnect", () => {
     console.log("Client disconnected");
